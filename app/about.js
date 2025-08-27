486a127ebae33623cd728e574c7046171d9c080d
Nestor ezekwe <nestorezekwec@gmail.com>
2025-08-27 16:06:50 +0100

    app discription

---
+++ app/about.js
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+
+export default function About () {
+    react (
+        <view>
+            <Text>About conpreneur</Text>
+            <Text>  AN app  for developer to collaborate with enterpreneurs</Text>
+        </view>
+    )
+}
